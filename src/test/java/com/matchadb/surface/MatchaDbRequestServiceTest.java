23158a7ff8ef760f349597c11e6225e93629300d
sammyreles22 <samuel.reles@gmail.com>
2021-01-18 22:08:00 -0500

    First push, test documentation

--- src/test/java/com/matchadb/surface/MatchaDbRequestServiceTest.java
+++ src/test/java/com/matchadb/surface/MatchaDbRequestServiceTest.java
@@ -1,5 +1,8 @@
 package com.matchadb.surface;
 
+import com.matchadb.models.MatchaDbRequestObject;
+import com.matchadb.models.MatchaDbResponseObject;
+
 import org.junit.jupiter.api.Test;
 import mockit.Tested;
 
@@ -12,41 +15,61 @@ public class MatchaDbRequestServiceTest {
 
     /**
      * Tests the usage of the conduct request method.
+     *
+     * This also tests successful runs of the four interpret methods.
      */
     @Test
     public void testConductRequest() {
+        // General declarations for the test
 
-    }
+        // Any expectations we need
 
-    /**
-     * Tests the usage of a GET request.
-     */
-    @Test
-    public void testInterpretGetResult() {
+        // We run a get method, have three objects returned
+        // We get what we need back from the interpret Get
+        // We return the completed object
 
-    }
+        // We run a post method, have one object inserted
+        // We get stuff back from the interpret Post
+        // We return the completed object
 
-    /**
-     * Tests the usage of a POST request.
-     */
-    @Test
-    public void testInterpretPostResult() {
+        // We run an update method, have two objects updated
+        // We get stuff back from the interpret Update
+        // We return the response object
         
+        // We run a delete method, have half the db deleted
+        // We get stuff back from the interpret Delete
+        // We return the completed object
     }
 
     /**
-     * Tests the usage of a UPDATE request.
+     * Tests interpreting GET request results.
      */
     @Test
-    public void testInterpretUpdateRequest() {
-
+    public void testInterpretGetFailedResult() {
+        // We provide back a response given that we had an unsuccessful get
     }
 
     /**
-     * Tests the usage of a DELETE request.
+     * Tests interpreting POST request results.
+     */
+    @Test
+    public void testInterpretPostFailedResult() {
+        // We provide back a response given that an insert (post) had failed
+    }
+
+    /**
+     * Tests interpreting UPDATE request results.
+     */
+    @Test
+    public void testInterpretUpdateFailedRequest() {
+        // We provide back a response when our update had failed
+    }
+
+    /**
+     * Tests interpreting DELETE request results.
      */
     @Test
     public void testInterpretDeleteRequest() {
-        
+        // We give back a response on our delete request upon it failing
     }
 }
