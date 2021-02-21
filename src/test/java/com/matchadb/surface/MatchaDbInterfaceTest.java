8ee5841d2c116c72c2b4331fc17cca0f52c8f3cc
sammyreles22 <samuel.reles@gmail.com>
2021-02-20 19:55:46 -0500

    Added constants class

--- src/test/java/com/matchadb/surface/MatchaDbInterfaceTest.java
+++ src/test/java/com/matchadb/surface/MatchaDbInterfaceTest.java
@@ -1,5 +1,14 @@
 package com.matchadb.surface;
 
+import static com.matchadb.common.MatchaDbConstants.SUCCESSFUL_GET_INFO;
+import static com.matchadb.common.MatchaDbConstants.SUCCESSFUL_POST_INFO;
+import static com.matchadb.common.MatchaDbConstants.SUCCESSFUL_UPDATE_INFO;
+import static com.matchadb.common.MatchaDbConstants.SUCCESSFUL_DELETE_INFO;
+import static com.matchadb.common.MatchaDbConstants.UNSUCCESSFUL_GET_INFO;
+import static com.matchadb.common.MatchaDbConstants.UNSUCCESSFUL_POST_INFO;
+import static com.matchadb.common.MatchaDbConstants.UNSUCCESSFUL_UPDATE_INFO;
+import static com.matchadb.common.MatchaDbConstants.UNSUCCESSFUL_DELETE_INFO;
+
 import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
@@ -12,29 +21,18 @@ import org.mockito.junit.jupiter.MockitoExtension;
  * class.
  */
 @ExtendWith(MockitoExtension.class)
-public class MatchaDbInterfaceTest {
-
-    // Copy-Pastad this over here from the Request Service, but it should be in a constants class
-    // or something.
-    // The info values for successful commands on the DB Table.
-    private final String SUCCESSFUL_GET_INFO = "Retrieval Successful";
-    private final String SUCCESSFUL_POST_INFO = "Insert Successful";
-    private final String SUCCESSFUL_UPDATE_INFO = "Update Successful";
-    private final String SUCCESSFUL_DELETE_INFO = "Removal Successful";
-
-    // The info values for unsuccessful commands on the DB Table.
-    private final String UNSUCCESSFUL_GET_INFO = "Retrieval Failed";
-    private final String UNSUCCESSFUL_POST_INFO = "Insert Failed";
-    private final String UNSUCCESSFUL_UPDATE_INFO = "Update Failed";
-    private final String UNSUCCESSFUL_DELETE_INFO = "Removal Failed";    
+public class MatchaDbInterfaceTest {  
 
     MatchaDbRequestParser matchaDbRequestParser = mock(MatchaDbRequestParser.class);
 
     MatchaDbInterface matchaDbInterface = new MatchaDbInterface(matchaDbRequestParser);
 
+    /**
+     * Tests that on a successful GET request that the interface provides values accordingly.
+     */
     @Test
     public void testGetRequestSuccessful() {
-
+        
     }
 
     @Test
