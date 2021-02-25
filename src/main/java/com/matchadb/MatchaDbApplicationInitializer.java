8b3b57cf18d591aa4ef24db431b78362117c084e
sammyreles22 <samuel.reles@gmail.com>
2021-02-24 21:40:30 -0500

    Trying different app initializers

---
+++ src/main/java/com/matchadb/MatchaDbApplicationInitializer.java
@@ -0,0 +1,13 @@
+package com.matchadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchaDbApplicationInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchaDbApplicationInitializer.class, args);
+    }
+
+}
