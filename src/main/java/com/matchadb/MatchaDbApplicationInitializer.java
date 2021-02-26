ed04173300f7496706fb2b82c8d8704e551de7b1
sammyreles22 <samuel.reles@gmail.com>
2021-02-25 23:16:57 -0500

    Testing some other stuff for component scanning

--- src/main/java/com/matchadb/MatchaDbApplicationInitializer.java
+++ src/main/java/com/matchadb/MatchaDbApplicationInitializer.java
@@ -2,8 +2,10 @@ package com.matchadb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.matchadb")
 public class MatchaDbApplicationInitializer {
 
     public static void main(String[] args) {
