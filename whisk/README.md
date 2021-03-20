ff675e48e847398d7d2db79aa86c6c9b175d75f8
sammyreles22 <samuel.reles@gmail.com>
2021-03-20 13:28:08 -0400

    Almost to close/merge

---
+++ whisk/README.md
@@ -0,0 +1,15 @@
+**Project Name**: MatchaDB
+
+**Summary**: _A simple DB implementation that can take in a json doc and develop an API from said JSON doc._
+
+**Status**: Beta Version Implemented
+
+**Recent Updates**:
+
+2021/03/20 - Implemented basic Whisk Application
+
+**System Requirements**
+
+Python 3: 
+
+    - Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 26 2016, 10:47:25)
--- whisk/whisk.py
+++ whisk/whisk.py
@@ -5,6 +5,7 @@ to use CLI interface, using Python 3, to run commands against the DB.
 import requests
 import sys
 
+# Different constants for the names of different commands used on Whisk.
 GET = "GET"
 POST = "POST"
 UPDATE = "UPDATE"
@@ -12,22 +13,34 @@ DELETE = "DELETE"
 HELP = "HELP"
 EXIT = "EXIT"
 
-protocol = "http://"   # Default is http
-host = "127.0.0.1"     # Default is localhost
-port = "8080"          # Default is port 8080
+# The current default protocol, http.
+PROTOCOL = "http://"   # Default is http
 
-# Retrieve Command and Info
+
 def retrieve_command():
+    '''
+    This method promts the user to provide a command, and then returns that command so that it can
+    be used by the rest of the application.
+    '''
     print("Please provide one of the following:\nGET, POST, UPDATE, DELETE, HELP, EXIT")
     print("Do note that this is not case sensitive.")
+
+    # Gather and return the command.
     return input("Your Command: ")
 
 
-# Help Command
 def help_command():
+    '''
+    Allows the user to get more information on different commands to better understand their 
+    workings and how to use them.
+    '''
     print("What command would you like more info on?\nGET, POST, UPDATE, DELETE, HELP, EXIT")
     print("This is not case sensitive.")
+
+    # Get the selected command.
     selected_command = input("> ").upper()
+
+    # Print help information accordingly.
     if (selected_command == GET):
         print("This command allows users to retrieve data from the DB.")
         print("Below are the promts provided with the GET command:")
@@ -56,17 +69,24 @@ def help_command():
         print("Just simply type exit when promted in any casing and you will exit the app.")
 
 
-# Run Get Command
 def get_command():
+    '''
+    This method collects the From and Select portions for a GET request, runs it against MatchaDB,
+    and prints the response code. It will also print exceptions, given that they occur.
+    '''
+    # Gather the From portion of the command.
     from_portion = "\"" + input("From: ") + "\""
 
-    part_one, part_two, part_three = input("Select: ").split()
-    select_portion = "[[\"" + part_one + "\", \"" + part_two + "\", \"" + part_three + "\"]]"
+    # Gather the Select portion of the command.
+    spart_one, spart_two, spart_three = input("Select: ").split()
+    select_portion = "[[\"" + spart_one + "\", \"" + spart_two + "\", \"" + spart_three + "\"]]"
 
+    # Develop the Parameter Values.
     parameter_vals = "{\"From\": " + from_portion + ", \"Select\": " + select_portion + "}"
 
     try:
-        response = requests.get(protocol + host + ":" + port + "/", data = str(parameter_vals))
+        # Make the request and see the response code.
+        response = requests.get(PROTOCOL + host + ":" + port + "/", data = repr(parameter_vals))
         print(response)
     except requests.exceptions.ConnectionError:
         print("A connection error has occured.")
@@ -74,16 +94,27 @@ def get_command():
         print("An unidentified error has occured of type " + type(e) + ".")
 
 
-
-# Run Post Command
 def post_command():
-    from_portion = input("From: ")
-    select_portion = input("Select: ")
+    '''
+    This method collects the From, Select, and Insert portions for a POST request, runs it against
+    MatchaDB, and prints the response code. It will also print exceptions, given that they occur.
+    '''
+    # Gather the From portion of the command.
+    from_portion = "\'" + input("From: ") + "\""
+
+    # Gather the Select portion of the command.
+    spart_one, spart_two, spart_three = input("Select: ").split()
+    select_portion = "[[\"" + spart_one + "\", \"" + spart_two + "\", \"" + spart_three + "\"]]"
+
+    # Gather the Insert portion of the command.
     insert_portion = input("Insert: ")
+
+    # Develop the Parameter Values.
     parameter_vals = {"From": from_portion, "Select": select_portion, "Insert": insert_portion}
 
     try:
-        response = requests.post(protocol + host + ":" + port + "/", data = str(parameter_vals))
+        # Make the request and see the response code.
+        response = requests.post(PROTOCOL + host + ":" + port + "/", data = repr(parameter_vals))
         print(response)
     except requests.exceptions.ConnectionError:
         print("A connection error has occured.")
@@ -91,15 +122,29 @@ def post_command():
         print("An unidentified error has occured of type " + type(e) + ".")
 
 
-# Run Update Command
 def update_command():
+    '''
+    This method collects the From, Select, and Update portions for a UPDATE (put) request, runs it 
+    against MatchaDB, and prints the response code. It will also print exceptions, given that they 
+    occur.
+    '''
+    # Gather the From portion of the command.
     from_portion = input("From: ")
-    select_portion = input("Select: ")
-    update_portion = input("Update: ")
+
+    # Gather the Select portion of the command.
+    spart_one, spart_two, spart_three = input("Select: ").split()
+    select_portion = "[[\"" + spart_one + "\", \"" + spart_two + "\", \"" + spart_three + "\"]]"
+
+    # Gather the Update portion of the command.
+    upart_one, upart_two, upart_three = input("Update: ").split()
+    update_portion = "[[\"" + upart_one + "\", \"" + upart_two + "\", \"" + upart_three + "\"]]"
+
+    # Develop the Parameter Values.
     parameter_vals = {"From": from_portion, "Select": select_portion, "Update": update_portion}
 
     try:
-        response = requests.put(protocol + host + ":" + port + "/", data = str(parameter_vals))
+        # Make the request and see the response code.
+        response = requests.put(PROTOCOL + host + ":" + port + "/", data = repr(parameter_vals))
         print(response)
     except requests.exceptions.ConnectionError:
         print("A connection error has occured.")
@@ -107,14 +152,24 @@ def update_command():
         print("An unidentified error has occured of type " + type(e) + ".")
 
 
-# Run Delete Command
 def delete_command():
+    '''
+    This method collects the From and Select portions for a DELETE request, runs it against 
+    MatchaDB, and prints the response code. It will also print exceptions, given that they occur.
+    '''   
+    # Gather the From portion of the command.
     from_portion = input("From: ")
-    select_portion = input("Select: ")
+
+    # Gather the Select portion of the command.
+    spart_one, spart_two, spart_three = input("Select: ").split()
+    select_portion = "[[\"" + spart_one + "\", \"" + spart_two + "\", \"" + spart_three + "\"]]"
+
+    # Develop the Parameter Values.
     parameter_vals = {"From": from_portion, "Select": select_portion}
 
     try:
-        response = requests.delete(protocol + host + ":" + port + "/", data = str(parameter_vals))
+        # Make the request and see the response code.
+        response = requests.delete(PROTOCOL + host + ":" + port + "/", data = repr(parameter_vals))
         print(response)
     except requests.exceptions.ConnectionError:
         print("A connection error has occured.")
@@ -122,29 +177,50 @@ def delete_command():
         print("An unidentified error has occured of type " + type(e) + ".")
 
 
+def main():
+    '''
+    The main method of the application.
+    '''
+    # Start of app
+    print("Welcome to Whisk, the MatchaDB Tester!")
+    print("SammaTime22, 2021")
 
-# Start of app
-print("Welcome to Whisk, the MatchaDB Tester!")
-print("SammaTime22, 2021")
+    # Set the variables of host and port to be global, and then set them
+    global host
+    global port
+    host = "127.0.0.1"     # Default is localhost
+    port = "8080"          # Default is port 8080
 
-# Get the hostname, if it exists
-if len(sys.argv) > 2:
-    host = sys.argv[1]
-if len(sys.argv) > 3:
-    port = sys.argv[2]
+    # Get the hostname, if it exists.
+    if len(sys.argv) > 2:
+        host = sys.argv[1]
 
-while True:
-    command_to_use = retrieve_command().upper()
+    # Get the portname, if it exists.
+    if len(sys.argv) > 3:
+        port = sys.argv[2]
 
-    if (command_to_use == GET):
-        get_command()
-    elif (command_to_use == POST):
-        post_command()
-    elif (command_to_use == UPDATE):
-        update_command()
-    elif (command_to_use == DELETE):
-        delete_command()
-    elif (command_to_use == HELP):
-        help_command()
-    elif (command_to_use == EXIT):
-        break
+    while True:
+        # Remind the user where the command is currently pointed at.
+        print("Using " + PROTOCOL + host + ":" + port + "/")
+
+        # Get the command.
+        command_to_use = retrieve_command().upper()
+
+        # Check which command this is for and run said command accordingly.
+        if (command_to_use == GET):
+            get_command()
+        elif (command_to_use == POST):
+            post_command()
+        elif (command_to_use == UPDATE):
+            update_command()
+        elif (command_to_use == DELETE):
+            delete_command()
+        elif (command_to_use == HELP):
+            help_command()
+        elif (command_to_use == EXIT):
+            break
+
+
+# Bootload the application.
+if __name__ == "__main__":
+    main()
