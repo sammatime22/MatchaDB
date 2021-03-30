2c031a18ca9847a36d73b02bd1b212ed35d8c17f
sammyreles22 <samuel.reles@gmail.com>
2021-03-30 09:00:54 -0400

    Found something else that doesn't work

--- src/main/java/com/matchadb/MatchaDbApplicationInitializer.java
+++ src/main/java/com/matchadb/MatchaDbApplicationInitializer.java
@@ -4,7 +4,10 @@ import com.matchadb.database.MatchaDbTable;
 
 import com.matchadb.loading.MatchaDbLoadingService;
 
-import java.time.LocalDateTime;
+import java.lang.Thread;
+import java.lang.InterruptedException;
+
+import java.time.LocalDateTime; 
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +27,17 @@ public class MatchaDbApplicationInitializer {
         logger.info(String.format("MatchaDB Initiated at %s", LocalDateTime.now()));
         SpringApplication.run(MatchaDbApplicationInitializer.class, args);
 
-        MatchaDbLoadingService loadingService = new MatchaDbLoadingService();
+        MatchaDbLoadingService loadingService = new MatchaDbLoadingService(); 
+
+        while (matchaDbTable == null) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                logger.error("Interrupted Exception thrown.", ie);
+            }
+            logger.info("Waiting on MatchaDbTable Initialization for loading...");
+        }
+
         loadingService.loadApplication(matchaDbTable);
 
         logger.info(String.format("MatchaDB Started at %s", LocalDateTime.now()));
